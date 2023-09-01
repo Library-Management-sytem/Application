0b0d0ddd8fc343c2607b027b0a379dbf12b7ca84
Sidati <sidatnouhi@gmail.com>
2023-09-01 16:49:08 +0100

    Updated sql file to insert more data and added some foreign keys

--- src/SQL.sql
+++ src/SQL.sql
@@ -1,4 +1,4 @@
-create database library;
+create database if not exists library;
 USE library;
 
 create table user
@@ -31,7 +31,11 @@ create table service (
     BookId int,
     BorrowDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     ReturnDate Date,
-    foreign key (bookId) references book(ISBN)
+    ClientId int,
+    UserId int,
+    foreign key (bookId) references book(ISBN),
+    foreign key (ClientId) references client(Id) ,
+    foreign key (UserId) references user(Id)
 );
 
 insert into user (Name,Email,Password,IsAdmin) values ('Sidati','sidati@gmail.com','sidati@123',true);
@@ -39,3 +43,4 @@ insert into book (ISBN, Name, Author, Year) values (111100, '1984', 'Jorge ORWEL
                                                    (111101, 'Animal farm', 'Jorge ORWELL', 1940),
                                                    (236789, 'Atomic habits', 'Flan ben flan', 2016),
                                                    (236789, 'How to become a human', 'Flan ben fertlan', 2023);
+insert into client (Name, Email) values ('Hassan','hassan@gmail.com');
